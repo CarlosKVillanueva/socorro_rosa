282da8e7e566ee29467f0629a660e5819cc1ae0a
Carlos K Villanueva <carloskvillanueva@gmail.com>
2021-09-21 00:08:24 -0300

    Final

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,13 +1,17 @@
-/* -------------------------------------------- */
-/*           Checkbox logic                     */
-/* -------------------------------------------- */
 
 $(".success__Box").hide();
 $(".success__Message").hide();
+
+/* -------------------------------------------------------------------------- */
+/*                               CHECKBOX LOGIC                               */
+/* -------------------------------------------------------------------------- */
+
+/*
 $('.js-year').on('change', function () {
     
     $year = $(this).val();
-    if ($(this).is(':checked')) {
+    console.log(year)
+    if ($(this).is(':selected')) {
         $('.subForm__' + $year).show();
 
         // $('#mudanzaGrupa-' + year).off('change');
@@ -24,11 +28,58 @@ $('.js-year').on('change', function () {
     } else {
         $('.subForm__' + $year).hide();
     }
+
+// 1. Agregamos si operan en otra provincia (SI/NO, no por deafult)
+// 2. Cambiar la seccion de años a "Año en el que se funda la grupa", y "Año de cese de actividades" (o sugerime otro titulo), y que cree 1 sub form por cada año entre medio.
+// 3. Al hacer enviar, pregunta si esta seguro que la grupa opera desde inicio a fin.
+// 4. Agregar un campo de notas no requerido al final, entre los años y el boton de enviar.
+5. Que al hacer enviar, valide que:
+//   a. Nombre de la grupa no sea vacio
+//   b. Provincia Tenga algun valor seleccionado
+//   c. para cada año visible que la cantidad de personas sea mayor a 0, y la localidad tenga algun valor seleccionado.
+
+
 });
 
-/* -------------------------------------------- */
-/*           Province logic                     */
-/* -------------------------------------------- */
+*/
+
+/* -------------------------------------------------------------------------- */
+/*                                SELECT LOGIC                                */
+/* -------------------------------------------------------------------------- */
+
+function disableForms(){
+    for (let i = 2012; i < 2021; i++) {
+        $('.subForm__' + i).hide();
+        $('.subForm__' + i).find('input').prop('disabled', true);
+    }
+}
+
+$('.fechaGrupa').on('change', function(){
+
+    $fundaYear = $('#fundaGrupa').val();
+    $ceseYear = $('#ceseGrupa').val();
+
+    disableForms();
+
+    for (let i = $fundaYear; i <= $ceseYear; i++) {
+        $('.subForm__' + i).show();
+        $('.subForm__' + i).find('input').prop('disabled', false);
+        $('#mudanzaGrupa-' + i).on('change', function (){
+            $(this).parents('.js-sub-form').find('.cuantasMudanza').toggle();
+            $(this).parents('.js-sub-form').find('.subForm__participa').toggle();
+            
+        });
+        $('#participaGrupa-' + i).on('change', function () {
+            $(this).parents('.js-sub-form').find('.dondeGrupa').toggle();
+        });
+        
+    }
+})
+
+/* -------------------------------------------------------------------------- */
+/*                               PROVINCE LOGIC                               */
+/* -------------------------------------------------------------------------- */
+
 $('.js-province-list').on('change', function(){
     $province = $('.js-province-list').val()
     if ($province != '') {
@@ -73,10 +124,17 @@ function defaultJsonData(){
     };
 }
 
+function yearChecked(year){
+    $fundaYear = $('#fundaGrupa').val();
+    $ceseYear = $('#ceseGrupa').val();
+    
+    return ($fundaYear <= year) && ( $ceseYear >= year);
+}
+
 function yearJson(year) {
     yearData = defaultJsonData();
 
-    if ($("#" + year).prop('checked')) {
+    if (yearChecked(year)) {
         yearData['integrantes'] = $('.subForm__' + year).find('[name=integrantesGrupa]').val();
         yearData['usuarios_mudaron'] = $('.subForm__' + year).find('[name=mudanzaGrupa]').val();
         yearData['catidad_mudanza'] = $('.subForm__' + year).find('[name=cuantasMudanza]').val();
@@ -89,7 +147,15 @@ function yearJson(year) {
 }
 
 function submitForm() {
-    data = {grupa: {nombre: $('#name').val(), provincia: $('#province option:selected').text(), years:[]}};
+    data = {
+        grupa: {
+            nombre: $('#name').val(), 
+            provincia: $('#province option:selected').text(), 
+            opera_otra_provincia: $("#operanProvincia").val(),
+            notas: $("#notas").val(),
+            years:[]
+        }
+    };
     for (let i = 2012; i < 2021; i++){
         year = {year: i, data: yearJson(i)}
         data['grupa']['years'].push(year)
@@ -106,20 +172,23 @@ function submitForm() {
       });
 }
 
-$("form").on('submit', function( event ) {
-    data = submitForm(this);
-    console.log(data)
-    // sendEmail();
-    event.preventDefault();
-
-    $("form").empty();
-
+$("form").on('submit', function( event ) { 
     
-    $(".success__Box").show();
-    $(".success__Message").show();
+    if(confirm('Esta segura que la grupa opera desde ' +$fundaYear+ ' hasta ' +  $ceseYear + '?')) {
+        data = submitForm(this);
+        // console.log(data)
+        event.preventDefault();
     
-    
-    
-    
+        $("form").empty();
+        
+        $(".success__Box").show();
+        $(".success__Message").show();
+        return true
+    }else{
+        return false
+    }
 });
-$("#form").trigger("reset"); //Line1
+$(document).on('ready', function(){
+    $("#form").trigger("reset");
+    disableForms();
+})
